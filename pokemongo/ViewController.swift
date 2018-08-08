31268e6509d79114da123b800f225a9689f15d0a
s911335 <r01543033@g.ntu.edu.tw>
2018-08-08 19:50:41 +0800

    write your meaaage

---
+++ pokemongo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  pokemongo
+//
+//  Created by 張凱翔 on 2017/11/17.
+//  Copyright © 2017年 Kai.Siang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
